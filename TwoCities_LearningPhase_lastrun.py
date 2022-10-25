c1987d2702ad2df9e84619aa839957b193a79936
zs-nemecz <zsuzska.nemecz.com>
2022-10-25 16:33:35 +0200

    student pilot day 3 - Pszichofiz lab

--- TwoCities_LearningPhase_lastrun.py
+++ TwoCities_LearningPhase_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2022.2.2),
-    on október 21, 2022, at 14:14
+    on október 25, 2022, at 15:08
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
