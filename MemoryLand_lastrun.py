1904af9d0cafcdc3419077587b207fd848a69feb
zs-nemecz <zsuzska.nemecz@gmail.com>
2022-12-13 16:20:16 +0100

    data: ST02_121322

--- MemoryLand_lastrun.py
+++ MemoryLand_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2022.2.4),
-    on December 13, 2022, at 13:18
+    on December 13, 2022, at 14:58
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
